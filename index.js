56788a202cc332d2b376dcdfa348ac82fdc72367
Jason Serafica <seraficaj@gmail.com>
2022-03-01 10:24:43 -0800

    sign up, login, and logout

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const ejsLayouts = require("express-ejs-layouts");
 const db = require("./models");
 const rowdy = require("rowdy-logger");
-const methodOverride = require('method-override');
+const methodOverride = require("method-override");
+const cookieParser = require("cookie-parser");
+const cryptoJS = require("crypto-js");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,7 +15,27 @@ app.use(require("morgan")("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use("/public", express.static("public"));
 app.use(ejsLayouts);
-app.use(methodOverride('_method'));
+app.use(cookieParser()); // gives access to req.cookies
+app.use(methodOverride("_method"));
+
+// COOKIE MIDDLEWARE
+// Custom Middleware
+app.use(async (req, res, next) => {
+    if (req.cookies.userId) {
+        // decrypting the incoming user id from the cookie
+        const decryptedId = cryptoJS.AES.decrypt(
+            req.cookies.userId,
+            process.env.SECRET
+        );
+        // converting the decrypted id into a readable string
+        const decryptedIdString = decryptedId.toString(cryptoJS.enc.Utf8);
+        // querying the db for the user with that id
+        const user = await db.user.findByPk(decryptedIdString);
+        // assigning the found user to res.locals.user in the routes, and user in the ejs
+        res.locals.user = user;
+    } else res.locals.user = null;
+    next(); // move on to next middleware
+});
 
 app.get("/", (req, res) => {
     db.project
@@ -30,7 +52,6 @@ app.get("/", (req, res) => {
 app.use("/projects", require("./controllers/projects"));
 app.use("/categories", require("./controllers/categories"));
 app.use("/users", require("./controllers/users"));
-
 
 app.get("*", (req, res) => {
     res.render("main/404");
